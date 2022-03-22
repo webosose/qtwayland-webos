168bc4df456979ef26634087c51bc908875baadc
Jungi Byun <jungi.byun@lge.com>
2022-03-22 15:30:32 +0900

    Rearrange methods in deliverUpdateRequest
    Change-Id: Ib4c43e8198284072842560a0418078e6bd2472d2

--- src/plugins/platforms/webos-wayland-egl/webosplatformwindow.cpp
+++ src/plugins/platforms/webos-wayland-egl/webosplatformwindow.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2021 LG Electronics, Inc.
+// Copyright (c) 2015-2022 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -217,14 +217,14 @@ void WebOSPlatformWindow::deliverUpdateRequest()
         ut->updateAnimationTimers(-1);
 #endif
 
-    QWaylandEglWindow::deliverUpdateRequest();
-
     if (enablePresentationTime) {
         WebOSPlatform *platform = WebOSPlatform::instance();
         WebOSPresentationTime *presentation = platform ? platform->presentation() : nullptr;
         if (presentation)
             presentation->requestFeedback(this);
     }
+
+    QWaylandEglWindow::deliverUpdateRequest();
 }
 
 void WebOSPlatformWindow::onOutputTransformChanged()
