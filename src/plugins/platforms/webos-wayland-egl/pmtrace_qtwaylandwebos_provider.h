cd5ee4f46f81115615cc6da2469ed3c57d33ccc7
sukil.hong <sukil.hong@lge.com>
2022-10-17 14:01:53 +0900

    Fix compilation with lttng-ust >= 2.13
    Change-Id: I9c8f344c011c1d1e1afe2abacdebe162345de0ca

--- src/plugins/platforms/webos-wayland-egl/pmtrace_qtwaylandwebos_provider.h
+++ src/plugins/platforms/webos-wayland-egl/pmtrace_qtwaylandwebos_provider.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2018 LG Electronics, Inc.
+// Copyright (c) 2016-2022 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,8 +17,8 @@
 #undef TRACEPOINT_PROVIDER
 #define TRACEPOINT_PROVIDER qtwayland_webos
 
-#undef TRACEPOINT_INCLUDE_FILE
-#define TRACEPOINT_INCLUDE_FILE ./pmtrace_qtwaylandwebos_provider.h
+#undef TRACEPOINT_INCLUDE
+#define TRACEPOINT_INCLUDE "./pmtrace_qtwaylandwebos_provider.h"
 
 #ifdef __cplusplus
 extern "C"{
